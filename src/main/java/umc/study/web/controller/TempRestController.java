8fe885964c09988edc30f7e3380c0f2abf542a2f
seongju jeong <seongjujeong@seongjuui-MacBookAir.local>
2023-12-20 15:41:00 +0900

    test

--- src/main/java/umc/study/web/controller/TempRestController.java
+++ src/main/java/umc/study/web/controller/TempRestController.java
@@ -7,13 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.study.apiPayload.ApiResponse;
 import umc.study.converter.TempConverter;
-import umc.study.domain.Mission;
-import umc.study.domain.enums.MissionStatus;
 import umc.study.service.TempService.TempQueryService;
 import umc.study.web.dto.TempResponse;
-
-import java.util.Arrays;
-import java.util.List;
 
 @RestController //해당 클래스가 RESTful 웹 서비스의 컨트롤러임을 나타냄. 각 메서드의 반환 값은 HTTP 응답으로 처리
 @RequestMapping("/temp") // 클래스 수준에서의 URL 매핑을 지정. 따라서 이 컨트롤러의 모든 메서드는 /temp 경로 아래에서 접근 가능
